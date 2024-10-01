3484028d9d13d58637e2f8bf508ab2f28766fcab
Azizur Rohman <azizurrohman@macbook-air.home>
2024-10-01 14:26:20 +0100

    Add all files and folders

---
+++ src/components/LoginSignup/LoginSignup.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import './LoginSignup.css'
+
+import user_icon from '../assets/user-solid.svg'
+import email_icon from '../assets/envelope-solid.svg'
+import password_icon from '../assets/lock-solid.svg'
+
+const LoginSignup = () => {
+
+  const [action, setAction] = useState("Sign Up");
+
+  return (
+    <div className='container'>
+      <div className='header'>
+        <div className='text'>{action}</div>
+        <div className='underline'></div>
+      </div>
+      <div className="inputs">
+        {action==="Login"?<div></div>: <div className="input">
+          <img src={user_icon} alt="" />
+          <input type="text" placeholder="Name"/>
+        </div>}
+       
+        <div className="input">
+          <img src={email_icon} alt="" />
+          <input type="email" placeholder="Email ID"/>
+        </div>
+        <div className="input">
+          <img src={password_icon} alt="" />
+          <input type="password" placeholder="Password"/>
+        </div>
+      </div>
+      {action==="Sign Up"?<div></div>:<div className="forgot-password">Lost password?<span>Click Here</span></div>}
+      
+      <div className="submit-container">
+        <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign up</div>
+        <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
+      </div>
+    </div>
+  )
+}
+
+export default LoginSignup
